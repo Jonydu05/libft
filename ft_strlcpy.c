f170ee05edccbecf5771fcc260b9a146ca4b4909
Jonydu (HOME) <joaog.zamp@gmail.com>
2026-06-14 05:12:38 -0300

    split and add strlcpy to libft.h

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,31 +1,8 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: jzampier <jzampier@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/03/09 18:22:56 by jzampier          #+#    #+#             */
-/*   Updated: 2026/03/10 21:15:42 by jzampier         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#include <libft.h>
 
-int				ft_strlen(char *str);
-unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size);
-
-int	ft_strlen(char *str)
+size_t ft_strlcpy(char *dest, char *src, size_t size)
 {
-	int	i;
-
-	i = 0;
-	while (str[i] != '\0')
-		i++;
-	return (i);
-}
-
-unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
-{
-	unsigned int	i;
+	size_t i;
 
 	i = 0;
 	while (i < size && src[i] != '\0')
--- libft.h
+++ libft.h
@@ -25,6 +25,7 @@ int		ft_atoi(char *str);
 int		ft_strncmp(char *s1, char *s2, unsigned int n);
 char    ft_tolower(char c);
 char	ft_toupper(char c);
+size_t	ft_strlcpy(char *dest, char *src, size_t size);
 size_t	ft_strlcat(char *dest, char *src, size_t size);
 
 #endif
