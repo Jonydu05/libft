6c40212c514e15c17aa9cd8268852ce4e83679ef
Jonydu (HOME) <joaog.zamp@gmail.com>
2026-06-14 05:08:42 -0300

    strjoin

--- libft.h
+++ libft.h
@@ -7,6 +7,7 @@ char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strnstr(char *str, char *to_find, size_t len);
 char	*ft_strdup(char *src);
+char	*ft_strjoin(int size, char **strs, char *sep);
 void    *ft_memset(void *mem, int x, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *mem, int ch, size_t count);
