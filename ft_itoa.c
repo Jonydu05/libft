82b9241929e8a4647533a7b9e7bb3c3624edb7a5
Joao Guilherme Zampieri  Preto Cardoso <jzampier@c1r6p1.42sp.org.br>
2026-06-30 21:06:16 -0300

    itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,14 +6,53 @@
 /*   By: jzampier <jzampier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/30 16:00:25 by jzampier          #+#    #+#             */
-/*   Updated: 2026/06/30 18:50:32 by jzampier         ###   ########.fr       */
+/*   Updated: 2026/06/30 21:05:34 by jzampier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+int	get_length(long n)
+{
+	int	len;
+
+	len = 0;
+	if (n <= 0)
+		len = 1;
+	if (n < 0)
+		n = n * -1;
+	while (n > 0)
+	{
+		n = n / 10;
+		len++;
+	}
+	return (len);
+}
+
 char	*ft_itoa(int n)
 {
-	char	*s;
+	char	*str;
+	long	nb;
+	int		len;
 
+	nb = n;
+	len = get_length(nb);
+	str = malloc((len + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	str[len] = '\0';
+	if (n == 0)
+		str[0] == '0';
+	if (nb < 0)
+	{
+		nb = nb * -1;
+		str[0] = '-';
+	}
+	while (nb > 0)
+	{
+		len--;
+		str[len] = (nb % 10) + '0';
+		nb = nb / 10;
+	}
+	return (str);
 }
