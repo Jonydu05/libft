90c57811e0c640ac2ab74aa25bd8322a5d0a7a01
Joao Guilherme Zampieri  Preto Cardoso <jzampier@c1r6p1.42sp.org.br>
2026-07-02 13:52:09 -0300

    str utils iterations

--- ft_striteri.c
+++ ft_striteri.c
@@ -1,32 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jzampier <jzampier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/06/30 21:08:06 by jzampier          #+#    #+#             */
-/*   Updated: 2026/07/02 13:40:30 by jzampier         ###   ########.fr       */
+/*   Created: 2026/07/02 13:41:17 by jzampier          #+#    #+#             */
+/*   Updated: 2026/07/02 13:49:55 by jzampier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
 	unsigned int	i;
-	char			*str;
 
 	if (!s || !f)
-		return (NULL);
-	str = malloc((ft_strlen(s) + 1) * sizeof(char));
-	if (!str)
 		return (NULL);
 	i = 0;
 	while (s[i] != '\0')
 	{
-		str[i] = f(i, s[i]);
+		f(i, &s[i]);
 		i++;
 	}
-	return (str);
+	return ;
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jzampier <jzampier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/30 15:52:26 by jzampier          #+#    #+#             */
-/*   Updated: 2026/07/01 21:06:54 by jzampier         ###   ########.fr       */
+/*   Updated: 2026/07/02 13:51:39 by jzampier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,16 +22,19 @@ char	*ft_strdup(char *src);
 char	*ft_strjoin(int size, char **strs, char *sep);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	*ft_memset(void *mem, int x, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *mem, int ch, size_t count);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_calloc(size_t num, size_t size);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_bzero(void *mem, size_t n);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
+void	ft_striteri(char *s, void (*f)(unsigned int, char *));
 int		ft_memcmp(const void *lhs, const void *rhs, size_t count);
 int		ft_isalnum(int num);
 int		ft_isalpha(int num);
